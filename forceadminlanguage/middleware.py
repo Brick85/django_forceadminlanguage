7a7a0ebfba5af1ad4583e02a1f9fc58b7acc4df0
Vital Belikov <vital@qwe.lv>
2013-01-15 15:22:47 +0200

    admin url

--- forceadminlanguage/middleware.py
+++ forceadminlanguage/middleware.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.utils import translation
+from django.core.urlresolvers import reverse
 
 
 class ForceAdminLanguageMiddleware:
 
     def process_request(self, request):
-        if request.path.startswith('/admin'):
+        if request.path.startswith(reverse('admin:index')):
             request.LANGUAGE_CODE = getattr(settings, 'ADMIN_LANGUAGE_CODE', settings.LANGUAGE_CODE)
             translation.activate(request.LANGUAGE_CODE)
             request.LANG = request.LANGUAGE_CODE
