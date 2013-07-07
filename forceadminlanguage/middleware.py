fc2e9c6c08fd81ee015fcc87c93d227d2b16d522
Evgeny Demchenko <evgeny@appannie.com>
2013-07-07 15:28:18 +0800

    Fixed path check

--- forceadminlanguage/middleware.py
+++ forceadminlanguage/middleware.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 class ForceAdminLanguageMiddleware:
 
     def process_request(self, request):
-        if request.path.startswith(getattr(settings, 'ADMIN_LANGUAGE_IGNORE_PATH', settings.ADMIN_LANGUAGE_IGNORE_PATH)):
+        if request.META['PATH_INFO'].startswith(getattr(settings, 'ADMIN_LANGUAGE_IGNORE_PATH', settings.ADMIN_LANGUAGE_IGNORE_PATH)):
             return
         if request.path.startswith(reverse('admin:index')):
             request.LANGUAGE_CODE = getattr(settings, 'ADMIN_LANGUAGE_CODE', settings.LANGUAGE_CODE)
